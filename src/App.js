5fa520e0742de00d3aa7e7816b31fa7f87389518
Tiffeney <tiffeney.rambo@gmail.com>
2018-10-18 16:02:08 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Form from './components/Form/Form';
+import ListCountainer from './components/ListContainer/ListContainer'
+import './App.css'
 
 class App extends Component {
+
+  state = {
+    item: "",
+    items: []
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    let { items, item } = this.state;
+    this.setState({ items: [...items, item ], item: "" })
+  }
+
+  handleChange = (e) => {
+    let { name, value } = e.target;
+    // console.log(e);
+    this.setState({ [name]: value })
+  }
+
   render() {
+    let { handleSubmit, handleChange } = this;
+    let { items, item } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container" style={{ marginTop: '20px' }}>
+        <div className="row">
+          <div className="column" style={{ textAlign: "center" }}>
+            <h1>Hello WishList!</h1>
+          </div>
+        </div>
+        <div className="row">
+          <div className="column">
+            <Form
+              handleSubmit={handleSubmit}
+              handleChange={handleChange} 
+              item={item}/>
+          </div>
+          <div className="column">
+            {items.length > 0
+            ? <ListCountainer items={items}/>
+            : <p>Awaiting Wishlist!</p>}
+          </div>
+        </div>
       </div>
     );
   }
